021019f452eb81ec86e1640d87be030bc43e2860
Rahim Packir Saibo <i.am@rah.im>
2014-07-30 11:07:30 +0100

    Added compiled script

---
+++ ember-segmentio.js
@@ -0,0 +1,50 @@
+Ember.SegmentioMixin = Ember.Mixin.create({
+  pageHasAnalytics: function() {
+    return window.analytics && typeof window.analytics === "object";
+  },
+
+  logTrackingEnabled: function() {
+    return !!window.ENV && !!window.ENV.LOG_EVENT_TRACKING;
+  },
+
+  logTracking: function() {
+    Ember.Logger.info('Tracking Segmentio.io event: ', arguments);
+  },
+
+  trackPageView: function(page) {
+    if (this.pageHasAnalytics()) {
+      if (!page) {
+        var loc = window.location;
+        page = loc.hash ? loc.hash.substring(1) : loc.pathname + loc.search;
+      }
+
+      analytics.page(page);
+    }
+
+    if (this.logTrackingEnabled()) {
+      this.logTracking('pageview', page);
+    }
+  },
+
+  trackEvent: function(event, properties, options, callback) {
+    if (this.pageHasAnalytics()) {
+      analytics.event(event, properties, options, callback);
+    }
+
+    if (this.logTrackingEnabled()) {
+      this.logTracking(event, properties, options);
+    }
+  }
+});
+
+Ember.Application.initializer({
+  name: "segmentio",
+
+  initialize: function(container, application) {
+    var router = container.lookup('router:main');
+    router.on('didTransition', function() {
+      this.trackPageView(this.get('url'));
+    });
+  }
+});
+Ember.Router.reopen(Ember.SegmentioMixin);
